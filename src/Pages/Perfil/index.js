1dccfb4aaa447f76356a7f9c00f18a86f0b818a7
Ricardo Malta <125774953+Ricardomms10@users.noreply.github.com>
2024-01-26 22:15:55 -0300

    atu2601

--- src/Pages/Perfil/index.js
+++ src/Pages/Perfil/index.js
@@ -1,20 +1,41 @@
-import { Text, View } from "react-native";
-
+import { Text, View, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function Perfil() {
 
     return (
-        <View>
-            <Text>MEU PERFIL</Text>
+        <View style={styles.container} >
+            <Text style={styles.textTitle}>MEU PERFIL</Text>
+
+            <TouchableOpacity>
+                <Icon name="user" solid />
+                <View>
+                    <Text>Ola, Ricardo</Text>
+                    <Text>Ricardomms10</Text>
+                    <Text>(13)98119-9007</Text>
+                </View>
+                <Icon name="angle-right" solid />
+            </TouchableOpacity>
+
+            <View></View>
 
             <View>
-                <Text>Ola, Ricardo</Text>
-                <Text>Ricardomms10</Text>
-                <Text>(13)98119-9007</Text>
+                <Text>MENSALIDADE</Text>
             </View>
 
             <View>
-                
+                <Text>DÚVIDAS FRENQUENTES</Text>
+            </View>
+
+            <View>
+                <Text>FALE COM A GENTE</Text>
+            </View>
+
+
+
+            <View>
+                <Text>SAIR</Text>
             </View>
         </View>
 
---
+++ src/Pages/Perfil/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+container:{
+    flex:1, 
+    marginTop:40,
+    
+},
+textTitle:{
+    textAlign:'center',
+    fontWeight:'bold',
+    fontSize:30,
+},
+
+
+
+})
+
+export default styles;
